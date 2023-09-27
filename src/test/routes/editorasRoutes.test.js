11251abfcccbe503e76b169943c9948d58571b98
KatiaSonally <katiasonally@yahoo.com.br>
2023-09-27 11:05:58 -0300

    feat: prepares for integration tests

---
+++ src/test/routes/editorasRoutes.test.js
@@ -0,0 +1,12 @@
+import { afterEach, beforeEach } from '@jest/globals';
+import app from '../../app.js';
+
+let server;
+beforeEach(() => {
+  const port = 3000;
+  server = app.listen(port);
+});
+
+afterEach(() => {
+  server.close();
+});
