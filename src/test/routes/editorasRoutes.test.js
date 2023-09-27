d1d2989b5941f4bd9c87406bcd79985e0fae6093
KatiaSonally <katiasonally@yahoo.com.br>
2023-09-27 14:21:27 -0300

    feat: implements the tests for edge cases

--- src/test/routes/editorasRoutes.test.js
+++ src/test/routes/editorasRoutes.test.js
@@ -38,20 +38,35 @@ describe('POST em /editoras', () => {
 
     idResposta = resposta.body.content.id;
   });
+  it('Deve não adicionar nada ao passar o body vazio', async () => {
+    await request(app)
+      .post('/editoras')
+      .send({})
+      .expect(400);
+  });
+});
+
+describe('GET em /editoras/id', () => {
+  it('Deve retornar o recurso selecionado', async () => {
+    await request(app)
+      .get(`/editoras/${idResposta}`)
+      .expect(200);
+  });
+});
+
+describe('PUT em /editoras/id', () => {
+  it('Deve alterar o campo nome', async () => {
+    await request(app)
+      .put(`/editoras/${idResposta}`)
+      .send({ nome: 'Casa do Código' })
+      .expect(204);
+  });
 });
 
 describe('DELETE em /editoras', () => {
   it('Deletar o recurso adicionado', async () => {
     await request(app)
       .delete(`/editoras/${idResposta}`)
-      .expect(200);
-  });
-});
-
-describe('GET em /editoras', () => {
-  it('Deve retornar o recurso selecionado', async () => {
-    await request(app)
-      .get(`/editoras/${idResposta}`)
       .expect(200);
   });
 });
