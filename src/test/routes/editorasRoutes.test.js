147fbe82c9965c10284a8264174c0b62f16d5632
KatiaSonally <katiasonally@yahoo.com.br>
2023-09-27 11:44:35 -0300

    feat: implements the integration tests

--- src/test/routes/editorasRoutes.test.js
+++ src/test/routes/editorasRoutes.test.js
@@ -1,4 +1,5 @@
-import { afterEach, beforeEach } from '@jest/globals';
+import request from 'supertest';
+import { describe } from '@jest/globals';
 import app from '../../app.js';
 
 let server;
@@ -9,4 +10,48 @@ beforeEach(() => {
 
 afterEach(() => {
   server.close();
+});
+
+describe('GET em /editoras', () => {
+  it('Deve retornar uma lista de editoras', async () => {
+    const resposta = await request(app)
+      .get('/editoras')
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200);
+
+    expect(resposta.body[0].email).toEqual('e@e.com');
+  });
+});
+
+let idResposta;
+describe('POST em /editoras', () => {
+  it('Deve adicionar uma nova editora', async () => {
+    const resposta = await request(app)
+      .post('/editoras')
+      .send({
+        nome: 'CDC',
+        cidade: 'São Paulo',
+        email: 's@s.com',
+      })
+      .expect(201);
+
+    idResposta = resposta.body.content.id;
+  });
+});
+
+describe('DELETE em /editoras', () => {
+  it('Deletar o recurso adicionado', async () => {
+    await request(app)
+      .delete(`/editoras/${idResposta}`)
+      .expect(200);
+  });
+});
+
+describe('GET em /editoras', () => {
+  it('Deve retornar o recurso selecionado', async () => {
+    await request(app)
+      .get(`/editoras/${idResposta}`)
+      .expect(200);
+  });
 });
