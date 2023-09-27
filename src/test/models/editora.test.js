4c2ae9926255082d72675907c62dfdd9f24c719e
KatiaSonally <katiasonally@yahoo.com.br>
2023-09-26 22:09:55 -0300

    feat: implements async function tests

--- src/test/models/editora.test.js
+++ src/test/models/editora.test.js
@@ -15,4 +15,29 @@ describe('Testando o modelo editora', () => {
       expect.objectContaining(objetoEditora),
     );
   });
+
+  it.skip('Deve salvar editora no BD', () => {
+    const editora = new Editora(objetoEditora);
+
+    editora.salvar().then((dados) => {
+      expect(dados.nome).toBe('CDC');
+    });
+  });
+
+  it('Deve salvar no BD usando a sintaxe moderna', async () => {
+    const editora = new Editora(objetoEditora);
+
+    const dados = await editora.salvar();
+
+    const retornado = await Editora.pegarPeloId(dados.id);
+
+    expect(retornado).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      }),
+    );
+  });
 });
